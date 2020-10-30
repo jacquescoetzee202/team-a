04d668cee5f929cc9bebb53b4f1f8b74d5422e18
tom <tomhall4321@gmail.com>
2020-10-30 14:22:20 +0000

    browser auto refreshes when work saved

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ let gulp = require('gulp');
 let cleanCSS = require('gulp-clean-css');
 let rename = require('gulp-rename');
 let sass = require('gulp-sass');
+let browserSync = require('browser-sync').create();
 
 gulp.task('minify-css' , () => {
     return gulp.src('css/styles.css')
@@ -17,10 +18,28 @@ gulp.task('sass', function () {
 		.pipe(gulp.dest('./css/'));
 });
 
+function reload(done) {
+browserSync.reload();
+done();
+}
+
+function serve(done) {
+browserSync.init({
+    server: {
+    baseDir: './'
+    }
+});
+done();
+}
+  
+
+
+const watching = () => gulp.watch(['./scss/**/*.scss', './js/*.js', 'index.html'], gulp.series('sass', 'minify-css', reload));
+  
+gulp.task('default', gulp.series(serve, watching));
+
 gulp.task('minify-sass' , gulp.series('sass' ,'minify-css'));
 
 gulp.task('sass-watch' , function () {
     return gulp.watch('scss/**/*.scss', gulp.series('minify-sass'));
 });
-
-
